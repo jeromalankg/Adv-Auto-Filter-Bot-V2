04a582cf7d1f7f1b1f98291360c66eb0bbe99957
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-05-03 20:28:01 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -107,7 +107,7 @@ async def auto_filter(bot, update):
             )
         
     else:
-        return # Couldn't find this movie.ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടത്തി അതുപോലെ ഇവിടെ ടൈപ്പ് ചെയ്യുക....[Google the original name of the movie and type it here ....]
+        return: Couldn't find this movie.ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടത്തി അതുപോലെ ഇവിടെ ടൈപ്പ് ചെയ്യുക....[Google the original name of the movie and type it here ....]
     
 
     if len(results) == 0: # double check
